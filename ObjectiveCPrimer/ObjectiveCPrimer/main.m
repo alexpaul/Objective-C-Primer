516969d875df2e99aac4b5086d74fc6d2baf42b2
Alex Paul <alexpaulnyc@gmail.com>
2019-08-05 06:38:03 -0400

    added objc xcode project

---
+++ ObjectiveCPrimer/ObjectiveCPrimer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveCPrimer
+//
+//  Created by Alex Paul on 8/5/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
