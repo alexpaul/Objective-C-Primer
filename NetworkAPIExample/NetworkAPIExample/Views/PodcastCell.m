72d8c911fd0ab2666fa328742c190da04a0cd595
Alex Paul <alexpaulnyc@gmail.com>
2019-08-08 22:40:39 -0400

    asset update

--- NetworkAPIExample/NetworkAPIExample/Views/PodcastCell.m
+++ NetworkAPIExample/NetworkAPIExample/Views/PodcastCell.m
@@ -12,13 +12,30 @@
 @property (weak, nonatomic) IBOutlet UIImageView *podcastImage;
 @property (weak, nonatomic) IBOutlet UILabel *collectionNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *artistNameLabel;
+@property (copy, nonatomic) NSString *urlString;
 @end
 
 @implementation PodcastCell
 
+- (void)prepareForReuse {
+  [super prepareForReuse];
+}
+
 - (void)configureCellWithPodcast: (Podcast *)podcast {
   self.collectionNameLabel.text = podcast.collectionName;
   self.artistNameLabel.text = podcast.artistName;
+  // process image data on background thread
+  self.urlString = podcast.artworkUrl600;
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:podcast.artworkUrl600]];
+    UIImage *image = [UIImage imageWithData:imageData];
+    // update UI on main thread
+    dispatch_async(dispatch_get_main_queue(), ^{
+      if ([self.urlString isEqualToString:podcast.artworkUrl600]) {
+        self.podcastImage.image = image;
+      }
+    });
+  });
 }
 
 @end
